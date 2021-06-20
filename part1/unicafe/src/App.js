2dcda33be71d2436ba647712529e9a4237b3550f
Lukas Lundin <lukasedv@outlook.com>
2021-06-20 19:33:20 +0000

    part 1.6 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const Header = () => {
+  return(
+    <div>
+      <h1>Give feedback</h1>
+    </div>
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = (props) => {
+  return(
+    <div>
+      <h2>Statistics</h2> 
+      <p>Good {props.stats[0]} </p>
+      <p>Neutral {props.stats[1]}</p>
+      <p>Bad {props.stats[2]}</p>
+    </div>
+  )
+}
 
 const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const stats = [good, neutral, bad]
+  console.log(stats)
+
   return (
     <div>
-      <h1>Hello Unicafe</h1>
+      <Header />
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <Statistics stats={stats}/>
     </div>
   )
 }
