e5836c41ef204c3686d4d0bcdb3432b2e69f6c5f
Lukas Lundin <lukasedv@outlook.com>
2021-06-22 17:22:41 +0000

    part 2.1 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Header = ({ course }) => {
+    return (
+      <h1>{course.name}</h1>
+    )
+  }
+  
+  const Total = ({ course }) => {
+    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+    return(
+      <p>Number of exercises {sum}</p>
+    ) 
+  }
+  
+  const Part = ({ name, exercises}) => {
+    return (
+      <p>
+        {name} {exercises}
+      </p>    
+    )
+  }
+  
+  const Content = ({ parts }) => {
+    return (
+      <div>
+          {parts.map(part =>
+            <Part key={part.id} name={part.name} exercises={part.exercises}/>
+            )}
+      </div>
+    )
+  }
 
-export default App;
+  const Course = ({ course }) => {
+    return (
+      <div>
+        <Header course={course} />
+        <Content parts={course.parts} />
+        <Total course={course} />
+      </div>
+    )
+  }
+  
+  const App = () => {
+    const course = {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+            name: 'Azure Fundamentals',
+            exercises: 2,
+            id: 4
+        }
+      ]
+    }
+  
+    return <Course course={course} />
+  }
+
+export default App
