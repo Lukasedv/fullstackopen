f3f71906cb1f1f8b6457f8aecf3cd9b2391cd6c9
Lukas Lundin <lukasedv@outlook.com>
2021-06-23 14:00:36 +0000

    part 2.8 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567'
+   }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const userExists = (name) => {
     return persons.some(function(el) {
@@ -12,10 +16,11 @@ const App = () => {
     }); 
   }
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
+      number: newNumber
     }
     console.log(newName);
     if(userExists(newName)) {
@@ -23,18 +28,22 @@ const App = () => {
     } else {
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
-  const Person = ({ name }) => {
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const Person = ({ name, number }) => {
     return(
       <div>
-        {name}
+        {name} {number}
       </div>
     )
   }
@@ -42,16 +51,26 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
+      <div>
+        Name 
         <input 
           value={newName}
           onChange={handleNameChange}
         />
+        </div>
+        <div>
+        Number
+        <input 
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+        </div>
         <button type="submit">save</button> 
       </form>
       <h2>Numbers</h2>
       {persons.map(person =>
-        <Person key={person.name} name={person.name} />
+        <Person key={person.name} name={person.name} number={person.number} />
         )}
     </div>
   )
