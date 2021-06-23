d199bdcf8fa9b703de1d9ad86fd5bfadd934807a
Lukas Lundin <lukasedv@outlook.com>
2021-06-23 21:07:47 +0000

    part 2.12 in progress

---
+++ part2/countries/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [ countries, setCountries] = useState([])
+  const [ searchCountry, setSearchCountry] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleSearch = (event) => {
+    setSearchCountry(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Countries</h1>
+      <input value={searchCountry} onChange={handleSearch} />
+      <div>
+      {countries.filter(country => country.name.toLowerCase().includes(searchCountry)).map(filteredCountry =>
+        <p key={filteredCountry.name}>{filteredCountry.name}</p>
+        )}
+        </div>
+    </div>
+  )
+}
+
+export default App
