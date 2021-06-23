0fee7ffdbf561051f0a9cf9e46761fd54d05c219
Lukas Lundin <lukasedv@outlook.com>
2021-06-23 19:51:27 +0000

    part 2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from "./components/Person"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchPerson, setSearchPerson] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const userExists = (name) => {
     return persons.some(function(el) {
@@ -48,7 +55,7 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchPerson(event.target.value)
   }
-  
+
   return (
     <div>
       <h2>Phonebook</h2>
