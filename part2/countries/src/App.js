47d9ae5f525a205872d6f6f3f47f3b8a2ab1a5e9
Lukas Lundin <lukasedv@outlook.com>
2021-06-27 09:03:13 +0000

    part 2.12 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const App = () => {
   const [ countries, setCountries] = useState([])
+  const [ filteredCountries, setFilteredCountries] = useState([])
   const [ searchCountry, setSearchCountry] = useState('')
 
   useEffect(() => {
@@ -12,21 +13,74 @@ const App = () => {
       .then(response => {
         console.log('promise fulfilled')
         setCountries(response.data)
+        setFilteredCountries(response.data)
       })
   }, [])
 
+  const Display = ({ countries }) => {
+    if(countries.length > 10) {
+      return(
+        <div>
+          Too many matches
+        </div>
+      )
+    } else if(countries.length === 1) {
+      return(
+        <div>
+        {countries.map(country => 
+          <Country country={country} format="long" />
+          )}
+        </div>)
+    } else {
+    return(
+      <div>
+        {countries.map(country => 
+          <Country country={country} format="short" />
+          )}
+      </div>
+    )
+  }
+  }
+
+  const Country = ({ country, format }) => {
+    if(format === "short") {
+    return(
+      <div>{country.name}</div>
+    )} else if(format === "long") {
+      return(
+      <div>
+        <h2>{country.name}</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <h3>Languages</h3>
+        <ul>
+        {country.languages.map(language => 
+          <li key={language.name}>{language.name}</li>
+        )}
+        </ul>
+        <img src={country.flag} width="100" alt={country.name}></img>
+
+      </div>
+      )
+    }
+  }
+
   const handleSearch = (event) => {
     setSearchCountry(event.target.value)
-  }
+    setFilteredCountries(countries.filter(country => country.name.toLowerCase().includes(searchCountry)))
+  }  
+
 
   return (
     <div>
       <h1>Countries</h1>
-      <input value={searchCountry} onChange={handleSearch} />
-      {countries.filter(country => country.name.toLowerCase().includes(searchCountry)).map(filteredCountry =>
-        <p key={filteredCountry.name}>{filteredCountry.name}</p>
-        )}
-      </div>
+      <input 
+        type="text"
+        value={searchCountry} 
+        onChange={handleSearch} 
+      />
+      <Display countries={filteredCountries}/>
+    </div>
   )
 
 }
