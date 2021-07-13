1ca1512169168be8e7fc70c8a26daa2821bbed5b
Lukas Lundin <lukasedv@outlook.com>
2021-07-13 10:53:27 +0000

    part 4.5 done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,12 @@ const dummy = (blogs) => {
     return blogs.reduce((accum,item) => accum + item.likes, 0)
   }
 
+  const favoriteBlog = (blogs) => {
+    return blogs.reduce((p, c) => p.likes > c.likes ? p : c)
+  }
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
