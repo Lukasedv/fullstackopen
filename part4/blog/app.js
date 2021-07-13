c78443172347b46c42d012a65e730c7945f41e75
Lukas Lundin <lukasedv@outlook.com>
2021-07-13 08:14:57 +0000

    works

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,44 +1,23 @@
-const http = require('http')
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
-require('dotenv').config()
-
-const MONGODB_URI = process.env.MONGODB_URI
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then(() => {
+  logger.info('Connected to DB')
 })
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-//const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.catch((error) => {
+  logger.error('error connecting', error.message)
+})
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = app
