e9a0156954c7e1967a9e61d70ddb633cae0e1597
Lukas Lundin <lukasedv@outlook.com>
2021-07-16 10:01:30 +0000

    part 4.8 done

---
+++ part4/blog/tests/test_helper.js
@@ -0,0 +1,33 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        "title": "This is a blog",
+        "author": "Lukas Lundin",
+        "url": "http:google.com",
+        "likes": 1
+      },
+      {
+        "title": "This is another blog",
+        "author": "Donald E",
+        "url": "http:google.com",
+        "likes": 3
+      }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ title: 'willremovethissoon', author: 'unknown', url: 'http', likes: 0 })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
