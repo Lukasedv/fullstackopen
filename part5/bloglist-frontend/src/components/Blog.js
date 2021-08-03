84c9f53886f647ba06a6ae1b597cdf14d6edcd18
Lukas Lundin <lukasedv@outlook.com>
2021-08-03 19:20:58 +0000

    part 5.7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,15 @@
-import React from 'react'
-import Togglable from './Togglable'
+import React, {useState} from 'react' 
 
 const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -10,14 +18,21 @@ const Blog = ({blog}) => {
     marginBottom: 5
   }
 
-  return(
-  <div style={blogStyle}>
-    {blog.title} {blog.author}
-  <Togglable buttonLabel='Show More'>
-    <p>Likes: {blog.likes}</p>
-    <p>Url: {blog.url}</p>
-  </Togglable>
-  </div>
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+      <p>{blog.title}
+        <button onClick={toggleVisibility}>Show</button>
+      </p>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.title}</p>
+        <p>{blog.author}</p>
+        <p>{blog.url}</p>
+        <p>Likes: {blog.likes} <button>Like</button> </p>
+        <button onClick={toggleVisibility}>Hide</button>
+      </div>
+    </div>
 )}
 
 export default Blog
