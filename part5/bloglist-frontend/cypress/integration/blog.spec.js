c6f0372cf2e4812ddb1d8f93b8a7422e0c1a8f0c
Lukas Lundin <lukasedv@outlook.com>
2021-08-05 11:36:07 +0000

    part 5.18 done

--- part5/bloglist-frontend/cypress/integration/blog.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog.spec.js
@@ -1,11 +1,41 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Lukas Lundin ',
+      username: 'lukasedv',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.visit('http://localhost:3000')
     cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('lukasedv')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Lukas Lundin logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('lukasedv')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.message')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Lukas Lundin logged in')
+    })
   })
 })
